3b78f1463d33ad6c9b544a0a57c5c21ccdef3105
valevo <valevogelmann@gmail.com>
2020-02-25 14:55:48 +0100

    restructured embeddings for the people

--- embeddings_for_the_people/raw_BERT/BERT_on_emails_on_LISA.py
+++ embeddings_for_the_people/raw_BERT/BERT_on_emails_on_LISA.py
@@ -7,32 +7,29 @@ Created on Mon Feb 24 17:58:57 2020
 """
 
 from transformers import DistilBertTokenizer, DistilBertModel, DistilBertConfig
-from transformers import BertTokenizer, BertModel, BertConfig
 
 import torch
-import numpy as np
 
 import pickle
 from tqdm import tqdm
-
-from collections import Counter
-import matplotlib.pyplot as plt
 
 import logging
 logging.getLogger("transformers.tokenization_utils").setLevel(logging.ERROR)
 
 
 
-
+# LOAD TOKENIZER AND MODEL
 tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
 model = DistilBertModel.from_pretrained('distilbert-base-uncased')
-
 model.eval()
 
 
+# WRAPPER FOR TOKENIZER CALL
 def email_to_ids(email_body):
     return torch.tensor(tokenizer.encode(email_body, add_special_tokens=True))
 
+
+# WRAPPER FOR MODEL CALL
 def email_to_vec(email_body_ids, to_id_first=True):
     if to_id_first:
         input_ids = email_to_ids(email_body_ids)
@@ -47,6 +44,8 @@ def email_to_vec(email_body_ids, to_id_first=True):
 #     return last_hidden_states.mean(0)  # mean vs sum
 
 
+# CUTS A LIST OF TOKENS IDs INTO CHUNKS OF 512 IF LONGER
+# ENSURES OVERLAP BETWEEN THE CHUNKS
 def cut_up(mail_tensor, n=511, k=20):
    unfolded = mail_tensor.unfold(0, n, n-k)
    covered = unfolded.shape[0]-unfolded.shape[1]
@@ -55,11 +54,10 @@ def cut_up(mail_tensor, n=511, k=20):
     
 
 
-import os
 
 
 if __name__ == "__main__":               
-    with open("emails_token_ids.pkl", "rb") as handle:
+    with open("../emails_token_ids.pkl", "rb") as handle:
         ids = pickle.load(handle)
     
     
