96285c6968a3cb8649c67d1aab2a59cb76511d52
valevo <valevogelmann@gmail.com>
2020-02-28 13:29:41 +0100

    first tries of personalised_GPT2 on LISA

--- embeddings_for_the_people/personalised_GPT2/run_on_LISA.sh
+++ embeddings_for_the_people/personalised_GPT2/run_on_LISA.sh
@@ -39,8 +39,13 @@ if ! test -f "W3CGPT2/full.test.all"; then
     cat W3CGPT2/full.test.raw.* > full.test.all
 fi
 
+if ! test -d "W3CGPT2/lm"; then
+    echo "W3CGPT2/lm does not exist; creating..."
+    mkdir W3CGPT2/lm
+fi
 
-python3 run_language_modeling.py --train_data_file=W3CGPT2/full.train.raw --model_type=gpt2 --output_dir=W3CGPT2 --model_name_or_path=gpt2 --do_train --line_by_line --num_train_epochs=2
+
+python3 run_language_modeling.py --train_data_file=W3CGPT2/full.train.raw --model_type=gpt2 --output_dir=W3CGPT2/lm --model_name_or_path=gpt2 --do_train --line_by_line --num_train_epochs=2
 
 cp -r $TMPDIR/embeddings_for_the_people/personalised_GPT2 $HOME
 
