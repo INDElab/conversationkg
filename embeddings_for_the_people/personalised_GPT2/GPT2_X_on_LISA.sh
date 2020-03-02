185dfa27f5ce1644e1eafdca7f031b8df4696dff
valevo <valevogelmann@gmail.com>
2020-03-02 17:50:02 +0100

    run on LISA

--- embeddings_for_the_people/personalised_GPT2/GPT2_X_on_LISA.sh
+++ embeddings_for_the_people/personalised_GPT2/GPT2_X_on_LISA.sh
@@ -31,7 +31,7 @@ cp -r $HOME/work/conversationkg/embeddings_for_the_people $TMPDIR
 cd $TMPDIR/embeddings_for_the_people/personalised_GPT2/
 
 
-for n in $(cat "GPT2_X/auth_names.txt"); do
+for n in $(cat "GPT2_X/auth_names2.txt"); do
     trainfile="GPT2_X/$n.train.raw"
     outdir="GPT2_X/lm_$n"
     
